34d5bd3895171c81fdcec79be5f4b5afcf600b5b
Robby Gillespie <rkgillespie@cs.uchicago.edu>
2021-02-18 12:39:34 -0600

    changes to crawler

--- play_caller.py
+++ play_caller.py
@@ -1,7 +1,14 @@
+# CS122: ACJR-Project
+#
+# Antony Awad, Cooper Powell, Johann Hatzius, Robert Gillespie
+
 import util_2
 import bs4
+
+'''
 def crawl_pages():
-    # Import modules
+
+    game_pages = []
 
     starting_url  = "https://www.pro-football-reference.com/years/"
     request = util_2.get_request(starting_url)
@@ -37,3 +44,44 @@ def crawl_pages():
             for game in games:
                 game_url = game.find_all("a")[0]["href"]
                 final_game_url = util_2.convert_if_relative_url(week_updated_url, game_url)
+                game_pages.apped(final_game_url)
+'''
+
+def get_game_pages():
+    game_pages = []
+    seasons_page, season_url = create_soup_object("https://www.pro-football-reference.com/years/")
+    years = seasons_page.find_all("th", scope = "row")
+    for year in years[0:11]:
+        year_url = find_url(year, season_url)
+        year_page, updated_year_url = create_soup_object(year_url)
+        ul = year_page.find_all("ul", class_="")
+        weeks = None
+        if years.index(year) < 3:
+            weeks = ul[12].find_all("li")
+        elif years.index(year) < 6:
+            weeks = ul[11].find_all("li")
+        else:
+            weeks = ul[10].find_all("li")
+        for li in weeks:
+            week_url = find_url(li, updated_year_url)
+            week_page, updated_week_url = create_soup_object(week_url)
+            games = week_page.find_all("td", class_="right gamelink")
+            for game in games:
+                game_url = find_url(game, updated_week_url)
+                game_page, _ = create_soup_object(game_url)
+                game_pages.append(game_page)
+    return game_pages
+
+
+
+def find_url(tag, current_url):
+    relative_url = tag.find_all("a")[0]["href"]
+    updated_url = util_2.convert_if_relative_url(current_url, relative_url)
+    return updated_url
+
+def create_soup_object(url):
+    request = util_2.get_request(url)
+    text = util_2.read_request(request)
+    updated_url = util_2.get_request_url(request)
+    soup = bs4.BeautifulSoup(text, "html5lib")
+    return soup, updated_url
