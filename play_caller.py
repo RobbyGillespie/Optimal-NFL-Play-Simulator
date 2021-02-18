12c0f05127a5719f46cc9c6326c16f9720767713
Cooper Powell <cooperpowell@cs.uchicago.edu>
2021-02-18 10:26:21 -0600

    i can't believe this works

--- play_caller.py
+++ play_caller.py
@@ -9,7 +9,7 @@ def crawl_pages():
     updated_url = util_2.get_request_url(request)
     soup = bs4.BeautifulSoup(text, "html5lib")
     years = soup.find_all("th", scope = "row")
-    for year in years[0:10]:
+    for year in years[0:11]:
         year_url = year.find_all("a")[0]["href"]
         abs_url = util_2.convert_if_relative_url(updated_url, year_url)
         abs_request = util_2.get_request(abs_url)
@@ -23,38 +23,17 @@ def crawl_pages():
         # for years 5-8, listheads[7], etc.
         ul = abs_soup.find_all("ul", class_="")
         weeks = None
-        if years.index(year) < 5:
-            weeks = ul[13].find_all("li")
-        else:
+        if years.index(year) < 3:
+            weeks = ul[12].find_all("li")
+        elif years.index(year) < 6:
             weeks = ul[11].find_all("li")
-        num = len(weeks)
-        week1 = weeks[0].find_all("a")[0]
-        week1_url = week1["href"]
-        week_urls = weeks_run(week1_url, 0, abs_updated_url, num, [])
-        for url in week_urls:
-            print(url)
-
-
-def weeks_run(week_url, num, year_url, total, urls): ##recursive function to return the list of all week urls from a given year
-    abs_week_url = util_2.convert_if_relative_url(year_url, week_url)
-    week_request = util_2.get_request(abs_week_url)
-    week_text = util_2.read_request(week_request)
-    week_soup = bs4.BeautifulSoup(week_text, "html5lib")
-    ul = week_soup.find_all("ul", class_="")
-    li = ul[11].find_all("li")
-    print(li)
-    urls = li[num].find_all("a")
-    print(urls)
-    url = urls[0]["href"]
-    if num == total - 1:
-        abs_url = util_2.convert_if_relative_url(year_url, url)
-        urls.extend(abs_url)
-    else:
-        num += 1
-        urls.extend(weeks_run(url, num, year_url, total, urls))
-    print(urls)
-    return urls
-
+        else:
+            weeks = ul[10].find_all("li")
+        for li in weeks:
+            a = li.find_all("a")[0]
+            url = a["href"]
+            week_url = util_2.convert_if_relative_url(abs_updated_url, url)
+            print(week_url)
 
 
 # General layout
