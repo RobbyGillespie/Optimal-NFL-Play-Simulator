b62d1ee15c012868ec7df05c07c4bd057777563f
Johann Hatzius <jhatzius@cs.uchicago.edu>
2021-03-08 01:20:39 -0600

    anti-coding monkey

--- play_caller.py
+++ play_caller.py
@@ -8,6 +8,7 @@ import scraper
 import numpy as np
 import pandas as pd
 
+
 def get_game_pages():
     game_pages = []
     seasons_page, season_url = create_soup_object("https://www.pro-football-reference.com/years/")
@@ -29,16 +30,21 @@ def get_game_pages():
             games = week_page.find_all("td", class_="right gamelink")
             for game in games:
                 game_url = find_url(game, updated_week_url)
-                game_pages.append(game_url)
+                season = year.text
+                game_pages.append((game_url, season))
     return game_pages
 
-def combine_games():
+def combine_games(year):
     game_pages = get_game_pages()
-    all_games = scraper.extractor(game_pages[0])
-    for url in game_pages[1:]:
-        game = scraper.extractor(url)
-        all_games = np.concatenate((all_games, game), axis=0)
-    return all_games
+    season_game_pages = [x[0] for x in game_pages if x[1] == str(year)]
+    game_list = []
+    for y in season_game_pages:
+        game_log = scraper.extractor(y)
+        game_list += game_log
+    df = pd.DataFrame(game_lst, columns = ['Quarter', 'Time', 'Down', \
+        'To go', 'Field position', 'Away score', 'Home score', \
+        'EPB', 'EPA', 'Team?', 'Play type', 'Direction', 'Yards gained'])
+    return df
 
 def find_url(tag, current_url):
     relative_url = tag.find_all("a")[0]["href"]
@@ -52,19 +58,12 @@ def create_soup_object(url):
     soup = bs4.BeautifulSoup(text, "html5lib")
     return soup, updated_url
 
-
-def create_pandas_df(master_array):
-    df = pd.DataFrame(master_array, columns = ['Quarter', 'Time', 'Down', \
-        'To go', 'Field position', 'Away score', 'Home score', \
-        'EPB', 'EPA' 'Team?', 'Play type', 'Direction', 'Yards gained'])
-
-
 def find_optimal_play(df, quarter, down, to_go, field_position, score_diff):
     same_quarter = df['Quarter'] == quarter
     same_down = df['Down'] == down
     same_to_go = find_yard_range(df['To go']) == find_yard_range(to_go)
-    same_field_position = 
-    same_score_diff = 
+    same_field_position = 0
+    same_score_diff = 0
     similar_plays = df[same_quarter & same_down & same_to_go & \
         same_field_position & same_score_diff]
     similar_plays_grouped = similar_plays.groupby['Play type'].size()
