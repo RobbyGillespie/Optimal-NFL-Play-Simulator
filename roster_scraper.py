a4926c3a0f10d39a4cc08cc0e06382fbaf521dfe
Johann Hatzius <jhatzius@cs.uchicago.edu>
2021-03-11 00:41:05 -0600

    roster

--- roster_scraper.py
+++ roster_scraper.py
@@ -16,20 +16,21 @@ from bs4 import BeautifulSoup,Comment
 YEAR=["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
 
 def crawl_roster():
-    roster_pages = {}
+    rosters = {}
     teams_page, teams_url = play_caller.create_soup_object("https://www.profootballarchives.com/teams.html")
     teams = teams_page.find_all("a")
-    for team in teams[0:32]:
+    for team in teams[8:40]:
+        rosters[team.text] = {}
         old_team_url = team["href"]
         team_url = util_2.convert_if_relative_url(teams_url, old_team_url)
         team_page, team_url = play_caller.create_soup_object(team_url)
         years = team_page.find_all("a", string=YEAR)
+        print(team.text)
         for year in years:
+            print(year.text)
             old_roster_url = year["href"]
             roster_url = util_2.convert_if_relative_url(team_url, old_roster_url)
-            inner = {}
-            inner[year] = roster_url
-            roster_pages[team] = inner
+            rosters[team.text][year.text] = find_players(roster_url)
     return roster_pages
 
 def extractor(link):
@@ -40,6 +41,10 @@ def extractor(link):
     soup = bs4.BeautifulSoup(document, "html5lib")
     table = soup.find_all('table')
     player_table = table[5].find_all('tr')
+    try:
+        dummy = int(player_table[2].contents[8].text)
+    except ValueError:
+        player_table = table[6].find_all('tr')
     player_list = []
     for a in player_table[2:]:
         player_info = a.contents
@@ -53,13 +58,12 @@ def find_players(link):
     '''
     player_df = extractor(link)
     player_dict = {}
-    starting_QB = player_df[player_df['Position'] == 'QB'].max()
-    starting_RB = player_df[player_df['Position'] == 'RB'].max()
-    starting_TE = player_df[player_df['Position'] == 'TE'].max()
-    WR_df = player_df[player_df['Position'] == 'WR']
-    starting_WR = WR_df.nlargest(3, ['GS'])
-    player_dict['QB'] = starting_QB['Name']
-    player_dict['RB'] = starting_RB['Name']
-    player_dict['TE'] = starting_TE['Name']
+    starting_QB = player_df[player_df['Position'] == 'QB'].nlargest(1, ['GS'])
+    starting_RB = player_df[player_df['Position'] == 'RB'].nlargest(1, ['GS'])
+    starting_TE = player_df[player_df['Position'] == 'TE'].nlargest(1, ['GS'])
+    starting_WR = player_df[player_df['Position'] == 'WR'].nlargest(3, ['GS'])
+    player_dict['QB'] = starting_QB['Name'].tolist()
+    player_dict['RB'] = starting_RB['Name'].tolist()
+    player_dict['TE'] = starting_TE['Name'].tolist()
     player_dict['WR'] = starting_WR['Name'].tolist()
     return player_dict
