75b51653ccff47ed786aa3d49ff475b3ce95815a
Robby Gillespie <rkgillespie@cs.uchicago.edu>
2021-03-12 03:04:08 -0600

    More changes for coop

--- simulator/mysite/simulator.py
+++ simulator/mysite/simulator.py
@@ -42,13 +42,16 @@ def create_optimal_plays(team_A_info, team_B_info, all_plays_df):
         (all_plays_df['Year'] == team_B_year)
     tBd_plays = all_plays_df[team_B_is_defense]
     tAo_means = tAo_plays.groupby(['Offense', 'Down', \
-        'To go category', 'Field zone', 'Play type']).mean()
+        'To go category', 'Field zone', 'Play type'])['EPC'].agg(['mean', 'count'])
     tBd_means = tBd_plays.groupby(['Defense', 'Down', \
-        'To go category', 'Field zone', 'Play type']).mean()
+        'To go category', 'Field zone', 'Play type'])['EPC'].agg(['mean', 'count'])
     tAo_v_tBd = pd.merge(tAo_means, tBd_means, on = ['Down', 'To go category', \
         'Field zone', 'Play type'])
-    sum_EPC = tAo_v_tBd['EPC_x'] + tAo_v_tBd['EPC_y']
+    sum_EPC = tAo_v_tBd['mean_x'] + tAo_v_tBd['mean_y']
     tAo_v_tBd['EPC_sum'] = sum_EPC
+    sum_weighted_EPC = ((tAo_v_tBd['count_x']**2) * tAo_v_tBd['mean_x']) + \
+        ((tAo_v_tBd['count_y']**2) * tAo_v_tBd['mean_y'])
+    tAo_v_tBd['sum_weighted_EPC'] = sum_weighted_EPC
     tA_optimal_plays = tAo_v_tBd[tAo_v_tBd['EPC_sum'] == \
         tAo_v_tBd.groupby(['Down', 'To go category', \
         'Field zone'])['EPC_sum'].transform(max)]
