1de73c63372a650b86d32a01d4768b8e90a8944c
Johann Hatzius <jhatzius@cs.uchicago.edu>
2021-03-11 19:14:23 -0600

    freedom

--- roster_scraper.py
+++ roster_scraper.py
@@ -51,11 +51,13 @@ def extractor(link):
             player_info = a.contents
             player_list.append([player_info[1].text, player_info[2].text,
                                 int(player_info[8].text)])
-        player_df = pd.DataFrame(player_list, columns=['Name', 'Position', 'GS'])
+        player_df = pd.DataFrame(player_list,
+                                 columns=['Name', 'Position', 'GS'])
         player_dict = find_players(player_df)
         return player_dict
     else:
-        return {'QB' : None, 'RB' : None, 'TE' : None, 'WR' : None}
+        return {'QB' : [None], 'RB' : [None],
+                'TE' : [None], 'K' : [None], 'WR' : [None]}
 
 def find_players(player_df):
     '''
@@ -71,15 +73,34 @@ def find_players(player_df):
     player_dict['TE'] = starting_TE['Name'].tolist()
     player_dict['K'] = starting_K['Name'].tolist()
     player_dict['WR'] = starting_WR['Name'].tolist()
+    if player_dict['RB'] == []:
+        starting_RB = player_df[player_df['Position'] == 'FB'].nlargest(1, ['GS'])
+        player_dict['RB'] = starting_RB['Name'].tolist()
+    if len(player_dict['QB']) == 0:
+        player_dict['QB'] = [None]
+    if len(player_dict['RB']) == 0:
+        player_dict['RB'] = [None]
+    if len(player_dict['TE']) == 0:
+        player_dict['TE'] = [None]
+    if len(player_dict['K']) == 0:
+        player_dict['K'] = [None]
+    if len(player_dict['WR']) == 0:
+        player_dict['WR'] = [None]
     return player_dict
 
 def dict_writer(dictionary):
     '''
     '''
-    with open('dict.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in dictionary.items():
-       writer.writerow([key, value])
+    x = pd.DataFrame.from_dict({(i, j, k): dictionary[i][j][k]
+                           for i in dictionary.keys()
+                           for j in dictionary[i].keys()
+                           for k in dictionary[i][j].keys()},
+                           orient='index', columns=['player1', 'player2', 'player3'])
+    x = x.reset_index()
+    x[['Name', 'Year', 'Pos']] = pd.DataFrame(x['index'].tolist(), index=x.index)
+    del x['index']
+    x.to_csv('rosters.csv')
+    return x
 
 '''
     w = csv.DictWriter(sys.stdout, ['years'] + YEAR)
