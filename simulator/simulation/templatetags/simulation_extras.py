26536892252ace3f54a5c0bbf313c1d0bef55466
Johann Hatzius <jhatzius@cs.uchicago.edu>
2021-03-12 13:15:17 -0600

    freedom

--- simulator/simulation/templatetags/simulation_extras.py
+++ simulator/simulation/templatetags/simulation_extras.py
@@ -9,10 +9,39 @@ def update_var(value):
     return value
 
 @register.simple_tag
+def cut_play_type(play_type):
+    '''cuts the play type to determine a play subtype'''
+    return play_type
+
+@register.simple_tag
 def find_player(player_list):
     '''returns a random player for the specific positions'''
     player = random.randint(0, len(player_list) - 1)
     return player_list[player]
+
+@register.simple_tag
+def pass_output(yards_gained):
+    '''outputs a string based on yardage for passes'''
+    if yards_gained == 'fumble':
+        return 'fumbled by'
+    elif yards_gained == 'interception':
+        return 'intercepted, intended for'
+    elif yards_gained == '0':
+        return 'incomplete to'
+    elif yards_gained == '1':
+        return 'for 1 yard to'
+    else:
+        return 'for ' + str(yards_gained) + ' yards to'
+
+@register.simple_tag
+def run_output(yards_gained):
+    '''outputs a string based on yardage for runs'''
+    if yards_gained == 'fumble':
+        return 'fumbled'
+    elif yards_gained == '1':
+        return 'for 1 yard'
+    else:
+        return 'for ' + str(yards_gained) + ' yards'
 
 @register.simple_tag
 def get_name(tup):
